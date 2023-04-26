27803228b9a2dec437fea5186807f9c127a7ccf8
zb_hai <hai_web@163.com>
2023-04-26 11:44:56 +0800

    初次提交

---
+++ src/js/styles~about~case~case_view~index~product~product_view~support.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{10:function(n,o,w){},7:function(n,o,w){}}]);
--- src/pages/case_view/index.js
+++
@@ -1,7 +0,0 @@
-import "../common/reset.css";
-import "./index.less";
-import "@/assets/global.less";
-
-// 引入页面公共部分的js
-import "../common/header";
-import "../common/footer";
